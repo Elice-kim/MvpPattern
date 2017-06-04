6c7069322b8593b64a7e74b593e7797d048cf543
elice-kim <grit_julia@naver.com>
2017-06-04 13:21:55 +0900

    add home activity

---
+++ MvpPatternApp/app/src/main/java/com/example/elice/mvppatternapp/home/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.example.elice.mvppatternapp.home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.elice.mvppatternapp.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
